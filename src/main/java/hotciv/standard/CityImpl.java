6106a43e8f90d5be71a96f9639f0b67a0b23da33
Oliver Medoc Benée Petersen <20186928@post.au.dk>
2019-09-11 19:35:19 +0200

    added field produced, and getProduction() now returns legion. setProduced header added removeFromTreasury method added added parameter to addToTreasury

--- src/main/java/hotciv/standard/CityImpl.java
+++ src/main/java/hotciv/standard/CityImpl.java
@@ -7,6 +7,7 @@ import java.security.acl.Owner;
 public class CityImpl implements City {
   private Player owner;
   private int treasury;
+  private String produced;
 
   public CityImpl(Player owner){
     this.owner = owner;
@@ -23,13 +24,17 @@ public class CityImpl implements City {
   public int getTreasury() {return treasury; }
 
   @Override
-  public String getProduction() {return "";}
+  public String getProduction() {return "legion";}
 
   @Override
   public String getWorkforceFocus() {return "";}
 
-  public void addToTreasury() {
-    treasury += 6;
+  public void addToTreasury(int amount) {
+    treasury += amount;
   }
+
+  public void setProduced(UnitImpl unitType){}
+
+  public void removeFromTreasury(int amount) {treasury -= amount;}
 }
 
